2d1dd6d960135eec3d04b8ac0d0819a772db7a5c
Сергей Зотов <sergey@Sergejs-MacBook-Air.local>
2025-12-19 03:56:12 +0300

    ViewsAndApp

---
+++ TaskManager4/Views/TaskEditorView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TaskManager4.Views;
+
+public partial class TaskEditorView : UserControl
+{
+    public TaskEditorView()
+    {
+        InitializeComponent();
+    }
+}
